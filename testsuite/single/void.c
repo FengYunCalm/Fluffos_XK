2a145355202f2b113d1481f3a498ca2624173f5f
FengYunCalm <218342008+FengYunCalm@users.noreply.github.com>
2026-06-14 00:52:05 +0800

    Register owner LPC domain tasks

--- testsuite/single/void.c
+++ testsuite/single/void.c
@@ -34,3 +34,21 @@ int owner_task_readonly()
 {
   return !vm_context_is_main_thread();
 }
+
+int owner_task_player() { return !vm_context_is_main_thread(); }
+int owner_task_room() { return !vm_context_is_main_thread(); }
+int owner_task_session() { return !vm_context_is_main_thread(); }
+int owner_task_item() { return !vm_context_is_main_thread(); }
+int owner_task_economy() { return !vm_context_is_main_thread(); }
+int owner_task_combat() { return !vm_context_is_main_thread(); }
+int owner_task_mail() { return !vm_context_is_main_thread(); }
+int owner_task_reward() { return !vm_context_is_main_thread(); }
+int owner_task_world() { return !vm_context_is_main_thread(); }
+int owner_task_persistence() { return !vm_context_is_main_thread(); }
+int owner_task_team() { return !vm_context_is_main_thread(); }
+int owner_task_guild() { return !vm_context_is_main_thread(); }
+int owner_task_sect() { return !vm_context_is_main_thread(); }
+int owner_task_quest() { return !vm_context_is_main_thread(); }
+int owner_task_rank() { return !vm_context_is_main_thread(); }
+int owner_task_crafting() { return !vm_context_is_main_thread(); }
+int owner_task_life_skill() { return !vm_context_is_main_thread(); }
